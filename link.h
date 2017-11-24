a43a4400c4315e696ba961466bf53b712f0a52f8
frieeze <alexis.surbayrole@isen.yncrea.fr>
2017-11-25 00:02:45 +0100

    avec le .h ca marche mieux

---
+++ link.h
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <math.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <stack>
+#include <string>
+
+
+int factorial(double, int);
+int displayG(std::vector<std::pair<int,double> >);
+double function_eval(std::vector<std::pair<int,double> >,double);
+void eval(std::string);
